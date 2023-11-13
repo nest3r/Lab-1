0b0430ade5b8be5b5dedcd8d4d8dee09e46fe1b6
nest3r <ernestandreev1@gmai.com>
2023-11-13 21:15:57 +0300

    commit

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
+#NUM 1
+
+GREEN = '\u001b[42m'
 RED = '\u001b[41m'
-BLUE = '\u001b[44m'
-WHITE = '\u001b[47m'
+YELLOW = '\u001b[43m'
 END = '\u001b[0m'
 
-'''for i in range(6):
-    if i < 3:
-        print(f'{BLUE}{"  " * (2 * i + 2)}{WHITE}{"  " * (14 - 2 * i)}{END}')
+for i in range(8):
+    if i < 4:
+        print(f'{GREEN}{"  " *6}{YELLOW}{"  " *8}{END}')
     else:
-        print(f'{BLUE}{"  " * (12 - 2 * i)}{RED}{"  " * (4 + 2 * i)}{END}')'''
+        print(f'{GREEN}{"  " *6}{RED}{"  " *8}{END}')
 
+#NUM 2
 
-plot_list = [[0 for i in range(10)] for i in range(10)]
-result = [0 for i in range(10)]
-
-for i in range(10):
-    result[i] = i ** 3
-
-step = round(abs(result[0] - result[9]) / 9, 2)
-print(step)
-
-for i in range(10):
-    for j in range(10):
-        if j == 0:
-            plot_list[i][j] = step * (8-i) + step
-
+RED = '\u001b[41m'
+WHITE = '\u001b[47m'
+END = '\u001b[0m'
 for i in range(9):
-    for j in range(10):
-        if abs(plot_list[i][0] - result[9 - j]) < step:
-            for k in range(9):
-                if 8 - k == j:
-                    plot_list[i][k+1] = 1
+    if i == 0 or i == 8:
+        print(f'{WHITE}{"  "*(11)}{END}')
+    elif i == 1 or i == 7:
+        print(f'{WHITE}{"  "*(3)}{RED}{"  "}{WHITE}{"  "*(3)}{RED}{"  "}{WHITE}{"  "*(3)}{END}')
+    elif i == 2 or i == 6:
+        print(f'{WHITE}{"  "*(2)}{RED}{"  "*(3)}{WHITE}{"  "}{RED}{"  "*(3)}{WHITE}{"  "*(2)}{END}')
+    elif i == 3 or i == 5:
+        print(f'{WHITE}{"  "}{RED}{"  "*(9)}{WHITE}{"  "}{END}')
+    elif i == 4:
+        print(f'{RED}{"  "*11}{END}')
 
-for i in range(9):
-    line = ''
-    for j in range(10):
-        if j == 0:
-            line += '\t' + str(int(plot_list[i][j])) + '\t'
-        if plot_list[i][j] == 0:
-            line += '--'
-        if plot_list[i][j] == 1:
-            line += '!!'
-    print(line)
-print('\t0\t1 2 3 4 5 6 7 8 9')
+#NUM 3
+RED = '\u001b[41m'
+WHITE = '\u001b[47m'
+END = '\u001b[0m'
+for i in range(8):
+    if i<7:
+        print(8-(i),f'{WHITE}{"  " * (8-(i+2))}{RED}{"  " * (1)}{WHITE}{"  " * (i)}{END}')
+    if i == 7:
+        print(1, ' 1 2 3 4 5 6 7 ')
 
-for i in range(10):
-    #print(plot_list[i])
-    pass
+#NUM 4
 
-file = open('sequence.txt', 'r')
-list = []
-for number in file:
-    list.append(float(number))
-file.close()
-print(list)
+f = open('sequence.txt')
+ls = []
+lsnew = []
+lsneww = []
+for num in f:
+    ls.append(float(num))
+for i in ls:
+    if i > -5 and  i < 0:
+        lsnew.append(i)
+    elif i > 0 and i < 5:
+        lsneww.append(i)
+f.close()
+print(len(lsneww)/len(lsnew))
